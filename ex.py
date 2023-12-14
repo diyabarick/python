09e9b592e2add9c7a64ad442d8da645bf1162e67
diyabarick <50700749+diyabarick@users.noreply.github.com>
2023-12-14 13:11:07 +0530

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,5 @@
+# take first middle and last and print initials 
+fst_name = input("please type in your first namev : " ) 
+mid_name = input("please type in your middle name : " )
+lst_name = input("please type in your last name : " )
+print ("your initials are " , fstname[:0], mid_name[:0], lst_name[:0])
